b6ff1b77125166d7c40540e9c1117444a6b6361f
Sean Policarpio <kdrakon@gmail.com>
2013-07-05 00:54:14 -0400

    majority of redesign completed

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,8 +1,6 @@
 var portfolioDefn = new Array();
 var slides = new Array();
-var slideIndex = 0;
 var slideSetIndex = 0;
-var lastTouchX = 0;
 
 function init(){
 	
@@ -13,130 +11,100 @@ function init(){
 		switch(e.keyCode){
 			case 39:
 				// right
-				shiftSlides(1, slideSetIndex);
+				//shiftSlides(1, slideSetIndex);
 				break;
 			case 37:
 				// left
-				shiftSlides(-1, slideSetIndex);			
+				//shiftSlides(-1, slideSetIndex);			
 				break;
 		}		
 	});
 	
-	// some touch mapping
-	var portfolioDiv = document.getElementById("portfolio");
-	portfolioDiv.addEventListener("touchmove", handleSwipe, false);
-}
-
-function handleSwipe(evt){
+	$('#slide-reset').hide().click(function(){
+		scrollTo(0,0);
+	});
 	
-	var touchlist = evt.changedTouches;
-	var swipeDirection = 0;
+	$(window).scroll(function(e){
+		if($(window).scrollLeft() != 0){
+			$('#slide-reset').show();
+		}else{
+			$('#slide-reset').hide();
+		}
+	});
 	
-	//for (var i = 0; i < touchlist.length; i++){
-		//var currentTouchX = touchlist[touchlist.length-1].clientX;
-		//if (currentTouchX < lastTouchX){
-			//// swiped left
-			//swipeDirection = -1;
-		//} else if (currentTouchX > lastTouchX) {
-			//// swiped right
-			//swipeDirection = 1;
-		//}
-		//lastTouchX = currentTouchX;
-	//}
-	
-	//uiShiftSlides(swipeDirection);
-		
 }
 
 // gets portfolio JSON and preloads all images
 function getPortfolioDefinition(jsonLocation){	
 
 	$.getJSON(jsonLocation, function(data){
-		portfolioDefn = data;
 		
-		// preload all sets
-		for (var i = 0; i < portfolioDefn.length; i++){
-			loadPortfolio(i);
-		}
+		portfolioDefn = data;
 		
 		// show first portfolio set	
 		slideSetIndex = 0;
-		showPortfolio(slideSetIndex);
+		loadPortfolio(slideSetIndex);
+		
+		// show titles/links to portfolio sets
+		showTitles();
 		
 	});		
-}
-
-function loadPortfolio(setIndex){
-	
-	slideSetIndex = setIndex;
-	slideIndex = 0;
-	
-	slides = new Array();	
-
-	for (var i = 0; i < portfolioDefn[slideSetIndex].images.length; i++){
-		slides[i] = new Image();
-		slides[i].src = portfolioDefn[slideSetIndex].images[i];
-	}	
-}
-
-function showPortfolio(setIndex){
-	
-	loadPortfolio(setIndex);
-
-	$(".slide").each(function(k, e){
-		var element = $(e);
-		if (k == 0){
-			element.attr('src', slides[k].src).removeClass("inactive-slide");
-		} else {
-			element.attr('src', slides[k].src).addClass("inactive-slide");
-		}
-		element.attr("onclick", "showImageModal('" + k + "')");
-		element.attr("id", "image_" + k);
-	});
-	
-	showTitles();	
 }
 
 function showTitles(){
 	var output = "";
 	$.each(portfolioDefn, function(k, e){
-		var link = "<a class='set-link' onclick='showPortfolio(" + k + ")'>" + e.title + "</a><br>";
+		var link = "<a class='set-link' onclick='loadPortfolio(" + k + ")'>" + e.title + "</a><br>";
 		output += link;
 	});
 	$("#set-titles").html(output);
 }
 
-// refactored helper method for frontend
-function uiShiftSlides(direction) {
-	shiftSlides(direction, slideSetIndex);
+function loadPortfolio(setIndex){
+	
+	slideSetIndex = setIndex;	
+	slides = new Array(portfolioDefn[slideSetIndex].images.length+1);	
+	
+	// clear all previous slides
+	$('#slide-set > .slide').remove();
+	
+	// move scroll back to the left
+	scrollTo(0,0);
+	
+	// reset the portfolio width
+	$("#portfolio").css("width", "0px");
+
+	// load and show images onload
+	for (var i = 0; i < portfolioDefn[slideSetIndex].images.length; i++){
+		slides[i] = new Image();
+		slides[i].src = portfolioDefn[slideSetIndex].images[i];
+		slides[i].onload = function(){
+			// show the image once its loaded
+			showImageInPortfolio(this);
+		};
+	}	
 }
 
-function shiftSlides(direction, setIndex){
+function showImageInPortfolio(image){
 	
-	slideIndex += direction;
-
-	if (slideIndex < 0) slideIndex = portfolioDefn[setIndex].images.length - 1;
-	if (slideIndex >= portfolioDefn[setIndex].images.length) slideIndex = 0;
+	var slideSetDiv = $('#slide-set');
+	var portfolioDiv = $('#portfolio');
+	var slideSetWidth = parseInt(portfolioDiv.css("width").replace("px", ""));
 	
-	var i;
-	$(".slide").each(function(k, e){
-		var element = $(e);
-		i = (slideIndex+k) % portfolioDefn[setIndex].images.length;
-		if (k == 0){
-			element.attr("src", slides[i].src).removeClass("inactive-slide");
-		} else {
-			element.attr("src", slides[i].src).addClass("inactive-slide");			
-		}
-		element.attr("onclick", "showImageModal('" + i + "')");
-		element.attr("id", "image_" + i);
-		
-		// shake them
-		element.animate({left: '+=' + direction*(k*4)});
-		element.animate({left: '+=' + direction*-1*(k*4)});
-	});
-		
+	// add/show the slide
+	$(image).addClass("slide");
+	slideSetDiv.append(image);
+	
+	// set the width of the portfolio div
+	var newWidth = $(image).position().left + image.width + slideSetWidth;
+	portfolioDiv.css("width", newWidth + "px");	
+	
+	// match the top-bar width with the portfolio width
+	//var barWidth = portfolioDiv.position().left + slideSetWidth;
+	$("#bar").css("width", newWidth + "px");
 }
 
+// deprecated
 function showImageModal(imageIndex){
 	
 	var image = $("#fullscreen-image");
@@ -155,6 +123,7 @@ function showImageModal(imageIndex){
 	image.show();	
 }
 
+// deprecated
 function hideImageModal(){
 	var image = $("#fullscreen-image");
 	image.hide();
